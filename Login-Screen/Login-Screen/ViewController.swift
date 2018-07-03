67f34a67cd6380749e1ebc6b7dc69e5578a5cfa5
Davis Booth <davis.booth@duke.edu>
2018-07-03 13:45:12 -0300

    Added server capabilities. Now possible to push to the server from the simulator in the app as well as automatically retrieve data for population in the table view in the main storyboard. SICK.

--- Login-Screen/Login-Screen/ViewController.swift
+++ Login-Screen/Login-Screen/ViewController.swift
@@ -7,18 +7,51 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    //Reference that links up the database
+    var ref: DatabaseReference!
+    
+    //Database Handle that specifies the listener connection
+    var databaseHandle: DatabaseHandle?
+    
     var postData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Set up Table View Controller
         tableView.delegate = self
-        tableView.dataSource = self;
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        
+        //Set firebase database reference
+        ref = Database.database().reference()
+        
+        //Retrieve the data to populate the cells from Firebase and listen for changes
+        ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            
+            
+            //Code to execute when a child is added in "Posts"
+            //Take the value from the snapshot and add it to the post data array at the top of this class
+            
+            //set post to just be the value of the snapshot (post data); attempts to assign the value to a String if not null
+            let post = snapshot.value as? String
+            
+            //testing to make sure there is data in post
+            if let actualPost = post {
+                //appends the post to the end of our post data array
+                self.postData.append(actualPost)
+                
+                //reloads table to refresh data
+                self.tableView.reloadData()
+            }
+        })
+
+        
     }
 
     override func didReceiveMemoryWarning() {
