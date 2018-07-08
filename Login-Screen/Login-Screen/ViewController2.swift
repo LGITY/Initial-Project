7e6b480c759dc71439887da7782ef36dc3661914
Brad Levin <balevin@mit.edu>
2018-07-08 13:43:58 -0400

    Allow registraition of users

--- Login-Screen/Login-Screen/ViewController2.swift
+++ Login-Screen/Login-Screen/ViewController2.swift
@@ -15,12 +15,15 @@ import Firebase
 
 class ViewController2:
 UIViewController {
+    
 
     @IBOutlet weak var signinSelector: UISegmentedControl!
     @IBOutlet weak var signinLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var signinButton: UIButton!
+    
+    
     
     var isSignIn:Bool = true
     //Boolean to check which part of the segmented control we are at
@@ -33,79 +36,121 @@ UIViewController {
         FirebaseApp.configure()
         return true
     }
-
+    
     override func viewDidLoad() {
-    super.viewDidLoad()
+        super.viewDidLoad()
+        if isSignIn {
+            print("signIn")
+            self.signinLabel?.text = "Sign In"
+            self.signinButton?.setTitle( "Sign In", for: .normal)
+        }
+        else {
+            print("register")
+            // if the signIn portion isn't toggled everything should say register
+            signinLabel?.text = "Register"
+            signinButton?.setTitle("Register", for: .normal)
+        }
+        
     }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
             
         }
     
-    @IBAction func signinSelectorChanged(_ sender: UISegmentedControl) {
+//    @IBAction func signinSelectorChanged(_ sender: UISegmentedControl) {
+//        print("suckadick")
+//
+//        //Switch it when the button is tapped
+//        isSignIn = !isSignIn
+//
+//        // Change the Sign in label and and button accordingly
+//        if isSignIn {
+//            signinLabel.text = "Sign In"
+//            signinButton.setTitle( "Sign In", for: .normal)
+//        }
+//        else {
+//            // if the signIn portion isn't toggled everything should say register
+//            signinLabel.text = "Register"
+//            signinButton.setTitle("Register", for: .normal)
+//        }
+        
+        
+        
+//    }
+    @IBAction func signinSelectorTapped(_ sender: Any) {
+        
         print("suckadick")
         
         //Switch it when the button is tapped
         isSignIn = !isSignIn
+        viewDidLoad()
         
         // Change the Sign in label and and button accordingly
-        if isSignIn {
-            signinLabel.text = "Sign In"
-            signinButton.setTitle( "Sign In", for: .normal)
-        }
-        else {
-            // if the signIn portion isn't toggled everything should say register
-            signinLabel.text = "Register"
-            signinButton.setTitle("Register", for: .normal)
-        }
-        
-        
-        
+       
     }
-    @IBAction func sigininButtonTapped(_ sender: UIButton) {
-        if let email = emailTextField?.text, let pass = passwordTextField?.text {
+    
+
+    @IBAction func signInTapped(_ sender: UIButton) {
+        print("herenow")
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+            
+            
             if isSignIn{
+                print("sdafjl")
                 
                 // sign into existing user on firebase
                 Auth.auth().signIn(withEmail: email, link: pass, completion: { (user, error) in
                     //makes sure the user isnt nill
                     
-                    if let u = user {
+                    if user != nil {
                         //found a user, go to the home screen
                         self.performSegue(withIdentifier: "goToHome", sender: self)
                         print("hello")
                     }
                     else {
+                        
                         print("goodbye")
                         //check error and show message
                     }
-                })
-            
-        }
-            
-        
-        else {
-            
-            // create a new user on firebase
+                }
+                )
                 
-            
-        Auth.auth().createUser(withEmail: email, password: pass, completion: { (user, error) in
-            
-            //check that user isnt nil
-            if let u = user {
-                //user is found go to home screen
-                print("heee")
-                self.performSegue(withIdentifier: "goToHome", sender: self)
             }
+                
+                
             else {
-                print("hooo")
-                // Error: check error and show message
+                
+                // create a new user on firebase
+                
+                print(email)
+                print(pass)
+                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                    // ...
+                    if let u = user {
+                        //user is found go to home screen
+                        print("heee")
+                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                    }
+                    else {
+                        print("hooo")
+                        // Error: check error and show message
+                    }
+                }
+                
             }
-            })
+            
         }
-}
+        
+    }
+    
+        
+    }
+    
 
-    }}
+
+
+
 
 
