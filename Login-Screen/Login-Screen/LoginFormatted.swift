2c8234c4c79294ff492a62569f827366074b53eb
Davis Booth <davis.booth@duke.edu>
2018-07-14 06:29:02 -0400

    Thought I could get the whole way through. I'm really close. I just basically have to add the OR and I have to figure out how to constrain the Dont have an account line to the center, which I am researching. Going to bed then working on this in the morning.

--- Login-Screen/Login-Screen/LoginFormatted.swift
+++ Login-Screen/Login-Screen/LoginFormatted.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class LoginFormatted: UIViewController {
     
+    //background and logo outlets
     @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var backgroundColor: UIView!
+    
+    //login label outlet
     @IBOutlet weak var loginLabel: UILabel!
+    
+    //username outlets
     @IBOutlet weak var usernameBox: UIView!
     @IBOutlet weak var usernameImage: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    //password outlets
+    @IBOutlet weak var passwordBox: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordImage: UIImageView!
+    
+    //facebook outlets
+    @IBOutlet weak var facebookBox: UIView!
+    @IBOutlet weak var facebookImage: UIImageView!
+    @IBOutlet weak var facebookButton: UIButton!
+    
+    
+    //log in button outlet
+    @IBOutlet weak var loginButton: UIButton!
+    
+    //forgot p, pp, and tos outlets
+    @IBOutlet weak var forgotPassword: UIButton!
+    @IBOutlet weak var privacyPolicy: UIButton!
+    @IBOutlet weak var termsOfService: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,21 +60,52 @@ class LoginFormatted: UIViewController {
         loginLabel.textColor = UIColor.white
         
         //load username
-        // TODO: Make function for this
-        usernameBox.backgroundColor = UIColor(red:0.11, green:0.17, blue:0.27, alpha:1)
-        usernameBox.layer.borderWidth = 12
-        usernameBox.layer.borderColor = UIColor(red:0.11, green:0.17, blue:0.27, alpha:1).cgColor
-        usernameBox.layer.cornerRadius = 15
-        usernameBox.layer.masksToBounds = true
+        loadTextView(usernameTextField, box: usernameBox, im: usernameImage)
         usernameImage.image = #imageLiteral(resourceName: "envelope")
-        usernameImage.contentMode = .scaleAspectFit
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: "email address", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        
+        //load password
+        loadTextView(passwordTextField, box: passwordBox, im: passwordImage)
+        passwordImage.image = #imageLiteral(resourceName: "lock")
+        passwordImage.contentMode = .scaleAspectFit
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        
+        //load login button
+        loginButton.layer.cornerRadius = 15
+        
+        //load facebook
+        //loads the surrounding box for the username
+        facebookBox.backgroundColor = UIColor(red:0.23, green:0.35, blue:0.6, alpha:1)
+        facebookBox.layer.borderWidth = 2
+        facebookBox.layer.borderColor = UIColor(red:0.11, green:0.17, blue:0.27, alpha:1).cgColor
+        facebookBox.layer.cornerRadius = 15
+        facebookBox.layer.masksToBounds = true
+        //loads the mail logo next to the username
+        facebookImage.contentMode = .scaleAspectFit
+        facebookImage.image = #imageLiteral(resourceName: "facebook")
+    }
+    
+    func loadTextView(_ textView: UITextField, box: UIView, im: UIImageView) {
+        //loads the surrounding box for the username
+        box.backgroundColor = UIColor(red:0.11, green:0.17, blue:0.27, alpha:1)
+        box.layer.borderWidth = 2
+        box.layer.borderColor = UIColor(red:0.11, green:0.17, blue:0.27, alpha:1).cgColor
+        box.layer.cornerRadius = 15
+        box.layer.masksToBounds = true
+        
+        //loads the mail logo next to the username
+        im.contentMode = .scaleAspectFit
+        
+        //loads the textfield for the username
+        textView.backgroundColor = UIColor.clear
+        textView.borderStyle = .none
     }
     
     func loadBackground() {
         //BACKGROUND
         //Sets up the background layer; creates a background rectangle, shades it in, and then adds the image in the background to fill the whole thing
         //let layer = UIView(frame: CGRect(x: -967, y: -214, width: 2337, height: 1547))
-        backgroundColor.backgroundColor = UIColor(red:0.03, green:0.12, blue:0.18, alpha:0.5)
+        backgroundColor.backgroundColor = UIColor(red:0.03, green:0.12, blue:0.18, alpha:0.7)
         //self.view.addSubview(layer)
         background.image = #imageLiteral(resourceName: "login")
         background.contentMode = .scaleAspectFill
