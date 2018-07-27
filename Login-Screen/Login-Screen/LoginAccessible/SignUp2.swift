630be282bf4aae3bdb793c86327be6b7c7f97505
Brad Levin <balevin@mit.edu>
2018-07-26 23:00:43 -0400

    minor change

--- Login-Screen/Login-Screen/LoginAccessible/SignUp2.swift
+++ Login-Screen/Login-Screen/LoginAccessible/SignUp2.swift
@@ -261,43 +261,43 @@ class SignUp2: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
 
     
     
-    @IBAction func facebookPressed(_ sender: Any) {
-        let fbLoginManager = FBSDKLoginManager()
-        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
-            if let error = error {
-                print("Failed to login: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let accessToken = FBSDKAccessToken.current() else {
-                print("Failed to get access token")
-                return
-            }
-            
-            let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
-            
-            // Perform login by calling Firebase APIs
-            Auth.auth().signIn(with: credential, completion: { (user, error) in
-                if let error = error {
-                    print("Login error: \(error.localizedDescription)")
-                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
-                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                    alertController.addAction(okayAction)
-                    self.present(alertController, animated: true, completion: nil)
-                    
-                    return
-                }
-                
-                // Present the main view
-                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
-                    UIApplication.shared.keyWindow?.rootViewController = viewController
-                    self.dismiss(animated: true, completion: nil)
-                }
-                
-            })
-            
-        }
-    }
+//    @IBAction func facebookPressed(_ sender: Any) {
+//        let fbLoginManager = FBSDKLoginManager()
+//        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+//            if let error = error {
+//                print("Failed to login: \(error.localizedDescription)")
+//                return
+//            }
+//
+//            guard let accessToken = FBSDKAccessToken.current() else {
+//                print("Failed to get access token")
+//                return
+//            }
+//
+//            let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+//
+//            // Perform login by calling Firebase APIs
+//            Auth.auth().signIn(with: credential, completion: { (user, error) in
+//                if let error = error {
+//                    print("Login error: \(error.localizedDescription)")
+//                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+//                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//                    alertController.addAction(okayAction)
+//                    self.present(alertController, animated: true, completion: nil)
+//
+//                    return
+//                }
+//
+//                // Present the main view
+//                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
+//                    UIApplication.shared.keyWindow?.rootViewController = viewController
+//                    self.dismiss(animated: true, completion: nil)
+//                }
+//
+//            })
+//
+//        }
+//    }
 }
 
 extension SignUp2:
