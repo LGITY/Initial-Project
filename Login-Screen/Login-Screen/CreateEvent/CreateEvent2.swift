1e7968fe6cb915198e1575bfc73b4f72e54287b7
Davis Booth <davis.booth@duke.edu>
2018-11-24 00:53:21 -0500

    sdf

--- Login-Screen/Login-Screen/CreateEvent/CreateEvent2.swift
+++ Login-Screen/Login-Screen/CreateEvent/CreateEvent2.swift
@@ -121,6 +121,7 @@ class CreateEvent2: UIViewController, UIPickerViewDataSource, UIPickerViewDelega
             self.friendList = tempArr
         })
         print(friendList)
+        print("Brad is ugly")
     }
     
     func loadGroups() {
